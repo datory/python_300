b3ec087eef63921f08f6bce549bcced0e5080273
datory <sehwan622@gmail.com>
2021-10-11 23:08:49 +0900

    013

--- 02_파이썬 변수.py
+++ 02_파이썬 변수.py
@@ -11,3 +11,21 @@ PER = 15.79
 print(시가총액)
 print(현재가)
 print(PER)
+
+# 013 문자열 출력
+# 변수 s와 t에는 각각 문자열이 바인딩 되어있습니다.
+# >> s = "hello"
+# >> t = "python"
+# 두 변수를 이용하여 아래와 같이 출력해보세요.
+# 실행 예:
+# hello! python
+
+# my code
+s = "hello"
+t = "python"
+print(s + "!" + " " + t)
+
+# answer
+s = "hello"
+t = "python"
+print(s+"!", t)
