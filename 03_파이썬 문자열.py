c7948a16b914cff6a4855510ae92a470d2a91d68
datory <sehwan622@gmail.com>
2021-11-05 23:58:42 +0900

    028

--- 03_파이썬 문자열.py
+++ 03_파이썬 문자열.py
@@ -79,3 +79,18 @@ print(url[-2:])
 url = "http://sharebook.kr"
 url_split = url.split('.')
 print(url_split[-1])
+
+
+
+# 028 문자열은 immutable
+# 아래 코드의 실행 결과를 예상해보세요.
+# >> lang = 'python'
+# >> lang[0] = 'P'
+# >> print(lang)
+lang = 'python'
+lang[0] = 'P'
+print(lang)
+# 문자열은 수정할 수 없습니다. 실행 결과를 확인해보면 문자열이 할당(assignment)메서드를 지원하지 않음을 알 수 있습니다.
+
+
+
