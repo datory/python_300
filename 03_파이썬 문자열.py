f8ca3c846208d2d23bd3ba0c81bb8f5406742bcd
datory <sehwan622@gmail.com>
2021-11-14 22:57:56 +0900

    044~045

--- 03_파이썬 문자열.py
+++ 03_파이썬 문자열.py
@@ -316,3 +316,20 @@ print(a)
 
 
 
+# 044 endswith 메서드
+# 파일 이름이 문자열로 저장되어 있을 때 endswith 메서드를 사용해서 파일 이름이 'xlsx'로 끝나는지 확인해보세요.
+# file_name = "보고서.xlsx"
+print("\n044 endswith 메서드")
+file_name = "보고서.xlsx"
+print(file_name.endswith("xlsx"))
+
+
+
+# 045 endswith 메서드
+# 파일 이름이 문자열로 저장되어 있을 때 endswith 메서드를 사용해서 파일 이름이 'xlsx' 또는 'xls'로 끝나는지 확인해보세요.
+# file_name = "보고서.xlsx"
+print("\n045 endswith 메서드")
+
+# the answer
+file_name = "보고서.xlsx"
+print(file_name.endswith(("xlsx", "xls")))
