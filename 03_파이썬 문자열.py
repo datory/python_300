d0c9f146d3c846926bc4b76a1801ac343c28789c
datory <sehwan622@gmail.com>
2021-11-16 00:00:03 +0900

    046

--- 03_파이썬 문자열.py
+++ 03_파이썬 문자열.py
@@ -333,3 +333,14 @@ print("\n045 endswith 메서드")
 # the answer
 file_name = "보고서.xlsx"
 print(file_name.endswith(("xlsx", "xls")))
+
+
+
+
+
+# 046 startswith 메서드
+# 파일 이름이 문자열로 저장되어 있을 때 startswith 메서드를 사용해서 파일 이름이 '2020'로 시작하는지 확인해보세요.
+# file_name = "2020_보고서.xlsx"
+print("\n046 startswith 메서드")
+file_name = "2020_보고서.xlsx"
+print(file_name.startswith("2020"))
