728a18f25b7d3bc434c5e7a62de6ee19c60b7490
datory <sehwan622@gmail.com>
2021-11-17 23:19:11 +0900

    047~050

--- 03_파이썬 문자열.py
+++ 03_파이썬 문자열.py
@@ -344,3 +344,43 @@ print(file_name.endswith(("xlsx", "xls")))
 print("\n046 startswith 메서드")
 file_name = "2020_보고서.xlsx"
 print(file_name.startswith("2020"))
+
+
+
+
+# 047 split 메서드
+# 다음과 같은 문자열이 있을 때 공백을 기준으로 문자열을 나눠보세요.
+# a = "hello world"
+print("\n047 split 메서드")
+a = "hello world"
+print(a.split(" "))
+
+
+
+# 048 split 메서드
+# 다음과 같이 문자열이 있을 때 btc와 krw로 나눠보세요.
+# ticker = "btc_krw"
+print("\n048 split 메서드")
+ticker = "btc_krw"
+print(ticker.split("_"))
+
+
+
+# 049 split 메서드
+# 다음과 같이 날짜를 표현하는 문자열이 있을 때 연도, 월, 일로 나눠보세요.
+# date = "2020-05-01"
+print("\n049 split 메서드")
+date = "2020-05-01"
+print(date.split("-"))
+
+
+
+# 050 rstrip 메서드
+# 문자열의 오른쪽에 공백이 있을 때 이를 제거해보세요.
+# data = "039490     "
+print("\n050 rstrip 메서드")
+data = "039490     "
+print(data.split())
+
+
+
