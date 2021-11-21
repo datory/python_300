1240fb79ad25fc76791920306606dea8cd0c6385
datory <sehwan622@gmail.com>
2021-11-21 22:56:55 +0900

    057~060

--- 04_파이썬 리스트.py
+++ 04_파이썬 리스트.py
@@ -90,3 +90,60 @@ langs = lang1 + lang2
 print(langs)
 
 
+
+# 057
+# 다음 리스트에서 최댓값과 최솟값을 출력하라. (힌트: min(), max() 함수 사용)
+# nums = [1, 2, 3, 4, 5, 6, 7]
+# 실행 예:
+# max:  7
+# min:  1
+print('057 리스트 최대값 최소값 출력')
+
+# my code
+nums = [1, 2, 3, 4, 5, 6, 7]
+print(min(nums))
+print(max(nums))
+
+# the answer
+nums = [1, 2, 3, 4, 5, 6, 7]
+print("max: ", max(nums))
+print("min: ", min(nums))
+
+
+
+# 058
+# 다음 리스트의 합을 출력하라.
+# nums = [1, 2, 3, 4, 5]
+# 실행 예:
+# 15
+print('058 리스트의 합 출력')
+
+# my code
+nums = [1, 2, 3, 4, 5]
+print("sum: ", sum(nums))
+
+# the answer
+nums = [1, 2, 3, 4, 5]
+print(sum(nums))
+
+
+
+
+# 059
+# 다음 리스트에 저장된 데이터의 개수를 화면에 구하하라.
+# cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+print('059 리스트 개수')
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+
+
+
+# 060
+# 다음 리스트의 평균을 출력하라.
+# nums = [1, 2, 3, 4, 5]
+# 실행 예:
+# 3.0
+print('060 리스트 평균')
+nums = [1, 2, 3, 4, 5]
+average = sum(nums) / len(nums)
+print(average)
