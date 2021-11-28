905ad7f411eaf685ac8299558311f8a08ba01dcd
datory <sehwan622@gmail.com>
2021-11-28 23:58:09 +0900

    067~070

--- 04_파이썬 리스트.py
+++ 04_파이썬 리스트.py
@@ -226,3 +226,52 @@ print(" ".join(interest))
 
 
 
+# 067 join 메서드
+# interest 리스트에는 아래의 데이터가 바인딩되어 있다.
+# interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+# interest 리스트를 사용하여 아래와 같이 화면에 출력하라.
+# 출력 예시:
+# 삼성전자/LG전자/Naver/SK하이닉스/미래에셋대우
+print('067 join 메서드')
+interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+print("/".join(interest))
+
+
+
+# 068 join 메서드
+# interest 리스트에는 아래의 데이터가 바인딩되어 있다.
+# interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+# join() 메서드를 사용해서 interest 리스트를 아래와 같이 화면에 출력하라.
+# 출력 예시:
+# 삼성전자
+# LG전자
+# Naver
+# SK하이닉스
+# 미래에셋대우
+print('068 join 메서드')
+interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+print("\n".join(interest))
+
+
+
+# 069 문자열 split 메서드
+# 회사 이름이 슬래시 ('/')로 구분되어 하나의 문자열로 저장되어 있다.
+# string = "삼성전자/LG전자/Naver"
+# 이를 interest 이름의 리스트로 분리 저장하라.
+# 실행 예시
+# >> print(interest)
+# ['삼성전자', 'LG전자', 'Naver']
+print('069 문자열 split 메서드')
+string = "삼성전자/LG전자/Naver"
+interest = string.split("/")
+print(interest)
+
+
+
+# 070 리스트 정렬
+# 리스트에 있는 값을 오름차순으로 정렬하세요.
+# data = [2, 4, 3, 1, 5, 10, 9]
+print('070 리스트 정렬')
+data = [2, 4, 3, 1, 5, 10, 9]
+data.sort()
+print(data)
