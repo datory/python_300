304b88312787b12e43701349543f482c8c2d779a
datory <sehwan622@gmail.com>
2021-11-29 23:19:16 +0900

    071~073

---
+++ 05_파이썬 튜플.py
@@ -0,0 +1,33 @@
+# 071
+# my_variable 이름의 비어있는 튜플을 만들라.
+print('071 튜플')
+my_variable = ()
+print(type(my_variable))
+
+
+
+# 072
+# 2016년 11월 영화 예매 순위 기준 top3는 다음과 같다. 영화 제목을 movie_rank 이름의 튜플에 저장하라. (순위 정보는 저장하지 않는다.)
+# 순위	영화
+# 1	닥터 스트레인지
+# 2	스플릿
+# 3	럭키
+print('072 튜플')
+movie_rank = ('닥터 스트레인지', '스플릿', '럭키')
+print(movie_rank)
+
+
+
+# 073
+# 숫자 1 이 저장된 튜플을 생성하라.
+print('073 튜플')
+
+# my code
+num = (1)
+print(num)
+
+# the answer
+my_tuple = (1)
+type (my_tuple)
+
+
