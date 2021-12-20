6ed5346856e90c9beaff76f3dfaabba62b1c4c87
datory <sehwan622@gmail.com>
2021-12-20 22:37:58 +0900

    090

--- 06_파이썬 딕셔너리.py
+++ 06_파이썬 딕셔너리.py
@@ -142,3 +142,22 @@ ice = {'메로나': 1000,
 del(ice['메로나'])
 print(ice)
 
+
+
+# 090
+# 다음 코드에서 에러가 발생한 원인을 설명하라.
+# >> icecream = {'폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+# >> icecream['누가바']
+# Traceback (most recent call last):
+#   File "<pyshell#69>", line 1, in <module>
+#     icecream['누가바']
+# KeyError: '누가바'
+print('090 딕셔너리')
+icecream = {'폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+icecream['누가바']
+
+# my answer
+# 키 값을 입력해 줘야한다.
+
+# the answer
+# 딕셔너리에 없는 키를 사용해서 인덱싱하면 에러가 발생합니다.
