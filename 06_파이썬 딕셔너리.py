d59979de7e829833462459ac0a0af997ad3630c3
datory <sehwan622@gmail.com>
2022-01-03 23:09:49 +0900

    097

--- 06_파이썬 딕셔너리.py
+++ 06_파이썬 딕셔너리.py
@@ -267,3 +267,18 @@ print(price)
 
 
 
+# 097 딕셔너리 values() 메서드
+# icecream 딕셔너리에서 아이스크림 판매 금액의 총합을 출력하라.
+# icecream = {'탱크보이': 1200, '폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+# 출력 예시:
+# 6700
+print('097 딕셔너리 values() 메서드')
+
+# my code
+icecream = {'탱크보이': 1200, '폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+allprice = sum(icecream.values())
+print(allprice)
+
+# the answer
+icecream = {'탱크보이': 1200, '폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+print(sum(icecream.values()))
