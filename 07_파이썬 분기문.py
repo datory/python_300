9afe19717343c66166561e23c7dd49eb341ef056
datory <sehwan622@gmail.com>
2022-01-12 21:05:41 +0900

    102

--- 07_파이썬 분기문.py
+++ 07_파이썬 분기문.py
@@ -1,6 +1,16 @@
 # 101
 # 파이썬에서 True 혹은 False를 갖는 데이터 타입은 무엇인가?
-print('100 zip과 dict')
+print('101')
 print('파이썬에서 True 혹은 False를 갖는 데이터 타입은 무엇인가?')
 print('bool type')
+
+
+
+# 102
+# 아래 코드의 출력 결과를 예상하라
+# print(3 == 5)
+print('102')
+print('아래 코드의 출력 결과를 예상하라')
+print('print(3 == 5)')
+print('False')
 
