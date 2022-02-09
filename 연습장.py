a4a7154ad005eb5eb3ef948f8104bc1641edd442
datory <sehwan622@gmail.com>
2022-02-09 20:45:40 +0900

    117

--- 연습장.py
+++ 연습장.py
@@ -1,9 +1,14 @@
-# my code
-user = input('현재시간:')
-user = user.split(':')
-minute = user[1]
-
-if minute == '00':
-    print('정각 입니다.')
-else:
-    print('정각이 아닙니다.')
+# 117
+# 사용자로 입력받은 단어가 아래 fruit 리스트에 포함되어 있는지를 확인하라. 포함되었다면 "정답입니다"를 아닐 경우 "오답입니다" 출력하라.
+# fruit = ["사과", "포도", "홍시"]
+# >> 좋아하는 과일은? 사과
+# 정답입니다.
+print('\n117')
+fruit = ["사과", "포도", "홍시"]
+user = input('좋아하는 과일은? ')
+if user == fruit[0]:
+    print('정답입니다.')
+elif user == fruit[1]:
+    print('정답입니다.')
+elif user == fruit[2]:
+    print('정답입니다.')
