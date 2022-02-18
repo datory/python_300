1e04dd3b06df670f867fcf237a83fae6bff5ba20
datory <sehwan622@gmail.com>
2022-02-18 23:20:19 +0900

    125

--- 07_파이썬 분기문.py
+++ 07_파이썬 분기문.py
@@ -498,3 +498,42 @@ elif num2 >= num1 and num2 >= num3:
 else:
     print(num3)
 
+
+
+# 125
+# 휴대폰 번호 앞자리에 따라 통신사는 아래와 같이 구분된다.
+# 사용자로부터 휴대전화 번호를 입력 받고, 통신사를 출력하는 프로그램을 작성하라.
+# 번호	통신사
+# 011	SKT
+# 016	KT
+# 019	LGU
+# 010	알수없음
+# >> 휴대전화 번호 입력: 011-345-1922
+# 당신은 SKT 사용자입니다.
+print('\n125')
+
+# my code
+user = input('휴대전화 번호 입력: ')
+user = user.split('-')
+print(user)
+if user[0] == '011':
+    print('당신은 SKT 사용자입니다.')
+elif user[0] == '016':
+    print('당신은 KT 사용자입니다.')
+elif user[0] == '019':
+    print('당신은 LGU 사용자입니다.')
+else:
+    print('당신은 알수없음 사용자입니다.')
+
+# the answer
+number = input("휴대전화 번호 입력: ")
+num = number.split("-")[0]
+if num == "011":
+    com = "SKT"
+elif num == "016":
+    com = "KT"
+elif num == "019":
+    com = "LGU"
+else:
+    com = "알수없음"
+print(f"당신은 {com} 사용자입니다.")
--- 연습장.py
+++ 연습장.py
@@ -1,17 +1,23 @@
-# 124
-# 사용자로부터 세 개의 숫자를 입력 받은 후 가장 큰 숫자를 출력하라.
-# >> input number1: 10
-# >> input number2: 9
-# >> input number3: 20
-# 20
-print('\n124')
-user1 = input('input number1: ')
-user2 = input('input number2: ')
-user3 = input('input number3: ')
-if user1 >= user2 and user1 >= user3:
-    print(user1)
-elif user2 >= user1 and user2 >= user3:
-    print(user2)
-elif user3 >= user1 and user3 >= user2:
-    print(user3)
-
+# 125
+# 휴대폰 번호 앞자리에 따라 통신사는 아래와 같이 구분된다.
+# 사용자로부터 휴대전화 번호를 입력 받고, 통신사를 출력하는 프로그램을 작성하라.
+# 번호	통신사
+# 011	SKT
+# 016	KT
+# 019	LGU
+# 010	알수없음
+# >> 휴대전화 번호 입력: 011-345-1922
+# 당신은 SKT 사용자입니다.
+print('\n125')
+number = input("휴대전화 번호 입력: ")
+num = number.split("-")[0]
+print(num)
+if num == "011":
+    com = "SKT"
+elif num == "016":
+    com = "KT"
+elif num == "019":
+    com = "LGU"
+else:
+    com = "알수없음"
+print(f"당신은 {com} 사용자입니다.")
