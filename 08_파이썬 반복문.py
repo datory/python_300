1789dc84b66f06d1b4d1a3145d1e370fae90aa85
datory <sehwan622@gmail.com>
2022-03-20 22:00:04 +0900

    137

--- 08_파이썬 반복문.py
+++ 08_파이썬 반복문.py
@@ -109,9 +109,24 @@ print('변환:', 'c')
 # print(변수)
 # 변수 = 30
 # print(변수)
+print('\n 136')
 for 변수 in [10, 20, 30]:
     print(변수)
 
 리스트 = [10, 20, 30]
 for 변수 in 리스트:
     print(변수)
+
+
+
+# 137
+# 다음 코드를 for문으로 작성하라.
+# print(10)
+# print(20)
+# print(30)
+print('\n 137')
+for 변수 in [10, 20, 30]:
+    print(변수)
+
+
+    
