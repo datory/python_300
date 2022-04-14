868a5a73db616f29e7df87a1df29cb67750d942f
datory <sehwan622@gmail.com>
2022-04-14 23:58:51 +0900

    158

--- 08_파이썬 반복문.py
+++ 08_파이썬 반복문.py
@@ -496,3 +496,18 @@ for 변수 in 리스트:
     print(변수.upper())
 
 
+
+# 158
+# 파일 이름이 저장된 리스트에서 확장자를 제거하고 파일 이름만 화면에 출력하라.
+# (힌트: split() 메서드)
+# 리스트 = ['hello.py', 'ex01.py', 'intro.hwp']
+# hello
+# ex01
+# intro
+print('\n158')
+리스트 = ['hello.py', 'ex01.py', 'intro.hwp']
+리스트[:].split(.)
+print(리스트)
+for 변수 in 리스트:
+    print(변수)
+
