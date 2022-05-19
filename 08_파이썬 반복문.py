afb2696580b19807797d89bc08f58b763f720adb
datory <sehwan622@gmail.com>
2022-05-19 23:59:25 +0900

    174

--- 08_파이썬 반복문.py
+++ 08_파이썬 반복문.py
@@ -842,3 +842,13 @@ for i in range(len(price_list)):
     print((len(price_list) - 1) - i, price_list[i])
 
 
+
+# 174
+# 아래와 같이 리스트의 데이터를 출력하라. 단, for문과 range문을 사용하라.
+# price_list = [32100, 32150, 32000, 32500]
+# 100 32150
+# 110 32000
+# 120 32500
+print('\n174')
+price_list = [32100, 32150, 32000, 32500]
+
