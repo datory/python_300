82263a4d7c2cfb640fd44a990393b968960687b4
datory <sehwan622@gmail.com>
2022-05-27 23:38:19 +0900

    177

--- 08_파이썬 반복문.py
+++ 08_파이썬 반복문.py
@@ -917,3 +917,17 @@ my_list = ["가", "나", "다", "라", "마"]
 for i in range(2, len(my_list) ) :
   print(my_list[i-2], my_list[i-1], my_list[i])
 
+
+
+# 177
+# 반복문과 range 함수를 사용해서 my_list를 아래와 같이 출력하라.
+# my_list = ["가", "나", "다", "라"]
+# 라 다
+# 다 나
+# 나 가
+print('\n177')
+my_list = ["가", "나", "다", "라"]
+for i in reversed(range(1, len(my_list))) :
+  print(my_list[i], my_list[i-1])
+
+
