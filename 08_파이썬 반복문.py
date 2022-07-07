8e9bd4af1e1b9fe93dcdb6c96f7ea3df5daa56e8
datory <sehwan622@gmail.com>
2022-07-07 23:46:40 +0900

    185

--- 08_파이썬 반복문.py
+++ 08_파이썬 반복문.py
@@ -1046,3 +1046,31 @@ print(stock)
 print('\n184')
 stock = {"10/10": [80, 110, 70, 90], "10/11": [210, 230, 190, 200] }
 print(stock)
+
+
+
+# 185
+# 리스트에 저장된 데이터를 아래와 같이 출력하라.
+# apart = [ [101, 102], [201, 202], [301, 302] ]
+# 101 호
+# 102 호
+# 201 호
+# 202 호
+# 301 호
+# 302 호
+print('\n185')
+
+# my code
+apart = [ [101, 102], [201, 202], [301, 302] ]
+print(apart[0][0], "호")
+print(apart[0][1], "호")
+print(apart[1][0], "호")
+print(apart[1][1], "호")
+print(apart[2][0], "호")
+print(apart[2][1], "호")
+
+# the answer
+apart = [ [101, 102], [201, 202], [301, 302] ]
+for row in apart:
+    for col in row:
+        print(col, "호")
