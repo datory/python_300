8690728b03da39c2cfbbb72e7e91ac7c3f4845d3
datory <sehwan622@gmail.com>
2022-07-27 22:18:58 +0900

    188

--- 08_파이썬 반복문.py
+++ 08_파이썬 반복문.py
@@ -1117,3 +1117,37 @@ apart = [ [101, 102], [201, 202], [301, 302] ]
 for row in apart[::-1]:
     for col in row[::-1]:
         print(col, "호")
+
+
+
+# 188
+# 리스트에 저장된 데이터를 아래와 같이 출력하라.
+
+# apart = [ [101, 102], [201, 202], [301, 302] ]
+# 101 호
+# -----
+# 102 호
+# -----
+# 201 호
+# -----
+# 202 호
+# -----
+# 301 호
+# -----
+# 302 호
+# -----
+print('\n188')
+
+# my code
+apart = [ [101, 102], [201, 202], [301, 302] ]
+for row in apart:
+  for col in row:
+    print(col, '호')
+    print('-----')
+
+# the answer
+apart = [ [101, 102], [201, 202], [301, 302] ]
+for row in apart:
+    for col in row:
+        print(col, "호")
+        print("-" * 5)
