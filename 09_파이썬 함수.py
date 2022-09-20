1ff6d6b38393d442207c20031fab9a3958ab1db0
datory <sehwan622@gmail.com>
2022-09-20 11:07:54 +0900

    209

--- 09_파이썬 함수.py
+++ 09_파이썬 함수.py
@@ -123,3 +123,17 @@ print('\n208')
 # D
 
 
+
+# 209
+# 아래 코드의 실행 결과를 예측하라.
+# def message1():
+#     print("A")
+# def message2():
+#     print("B")
+#     message1()
+# message2()
+print('\n208')
+
+# my answer
+# B
+# A
