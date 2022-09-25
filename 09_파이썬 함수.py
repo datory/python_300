00e53fb722ade986bd62c17079e7178297659b4c
datory <sehwan622@gmail.com>
2022-09-26 00:14:26 +0900

    213

--- 09_파이썬 함수.py
+++ 09_파이썬 함수.py
@@ -192,3 +192,17 @@ print('\n212')
 # 15
 
 
+
+# 213
+# 아래와 같은 에러가 발생하는 원인을 설명하라.
+# def 함수(문자열) :
+#     print(문자열)
+# 함수()
+# TypeError: 함수() missing 1 required positional argument: '문자열'
+print('\n213')
+
+# my answer
+# String 은 ""를 붙여줘야 한다.
+
+# the anwer
+# 함수에 정의와 다르게 함수를 호출하고 있다. 함수를 호출할 때 하나의 파라미터를 입력해야한다.
