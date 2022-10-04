a7ca5d5c36c7e1088f8bf05e624f3a49a867951e
datory <sehwan622@gmail.com>
2022-10-04 23:59:46 +0900

    220

--- 09_파이썬 함수.py
+++ 09_파이썬 함수.py
@@ -304,3 +304,20 @@ def print_arithmetic_operation(a, b):
     print(a, "/", b, "=", a / b)
 print_arithmetic_operation(3, 4)
 
+
+
+# 220
+# 세 개의 숫자를 입력받아 가장 큰수를 출력하는 print_max 함수를 정의하라.
+# 단 if 문을 사용해서 수를 비교하라.
+print('\n220')
+
+# my code
+def print_max(a, b, c):
+    if a < b and a< c:
+        print(a)
+    elif b < a and b < c:
+        print(b)
+    else :
+        print(c)
+
+print_max(3, 2, 1)
