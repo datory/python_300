d50a2515ec6fa43b67408cf7ecfbf76d69137f7b
datory <sehwan622@gmail.com>
2022-10-23 11:38:35 +0900

    227

--- 09_파이썬 함수.py
+++ 09_파이썬 함수.py
@@ -482,3 +482,7 @@ def print_mxn(string, num):
 
 
 # the answer
+def print_mxn(line, num):
+    chunk_num = int(len(line) / num)
+    for x in range(chunk_num + 1) :
+        print(line[x * num: x * num + num])
