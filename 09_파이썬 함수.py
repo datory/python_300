48650a7a39656eb30de822226bad35442d8b915f
datory <sehwan622@gmail.com>
2022-11-03 14:31:42 +0900

    230

--- 09_파이썬 함수.py
+++ 09_파이썬 함수.py
@@ -532,3 +532,23 @@ def my_print (a, b) :
 
 my_print(a=100, b=200)
 
+
+
+# 230
+# 아래 코드의 실행 결과를 예측하라.
+# def my_print (a, b) :
+#     print("왼쪽:", a)
+#     print("오른쪽:", b)
+
+# my_print(b=100, a=200)
+print('\n230')
+
+# my code
+# 왼쪽: 200
+# 오른쪽: 100
+
+def my_print (a, b) :
+    print("왼쪽:", a)
+    print("오른쪽:", b)
+
+my_print(b=100, a=200)
