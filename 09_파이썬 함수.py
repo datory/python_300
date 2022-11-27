5cefbfe1abc48046fc1ca9806aa2bf3eb690a229
datory <sehwan622@gmail.com>
2022-11-27 23:46:01 +0900

    236

--- 09_파이썬 함수.py
+++ 09_파이썬 함수.py
@@ -680,3 +680,16 @@ print(convert_int("1,234,567"))
 
 
 
+# 236
+# 아래 코드의 실행 결과를 예측하라.
+
+# def 함수(num) :
+#     return num + 4
+
+# a = 함수(10)
+# b = 함수(a)
+# c = 함수(b)
+# print(c)
+print('\n236')
+
+
