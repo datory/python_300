31a7c32cbf0aca3e83a5cacb53005e5a18166e04
datory <sehwan622@gmail.com>
2023-01-21 23:51:16 +0900

    242

--- 10_파이썬 모듈.py
+++ 10_파이썬 모듈.py
@@ -7,3 +7,21 @@ import datetime
 
 now = datetime.datetime.now()
 print(now)
+
+
+
+# 242 현재시간의 타입
+# datetime 모듈의 now 함수의 리턴 값의 타입을 화면에 출력해보세요.
+print('\n242')
+
+# my answer
+import datetime
+
+now = datetime.datetime.now()
+print(type(now))
+
+# the answer
+import datetime
+
+now = datetime.datetime.now()
+print(now, type(now))
