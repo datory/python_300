6c7a0254e166be03ead307781b54f8b29bdcc122
datory <sehwan622@gmail.com>
2023-01-26 00:04:35 +0900

    245

--- 10_파이썬 모듈.py
+++ 10_파이썬 모듈.py
@@ -58,3 +58,22 @@ print('\n244')
 import datetime
 now = datetime.datetime.now()
 print(now.strftime('%H:%M:%S'))
+
+
+
+# 245 strptime
+# datetime.datetime.strptime 메서드를 사용하면 문자열 형식의 시간을 datetime.datetime 타입의 시간 값으로 만들어줍니다.
+# "2020-05-04"의 문자열을 시간 타입으로 변환해보세요.
+print('\n245')
+
+# my code
+# import datetime
+# print(datetime.datetime.strptime("2020-05-04"))
+
+
+# the answer
+import datetime
+
+day = "2020-05-04"
+ret = datetime.datetime.strptime(day, "%Y-%m-%d")
+print(ret, type(ret))
